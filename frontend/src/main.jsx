fd9878d6df8476bd31585044698bd31b8b9fe532
pawan pawar <pavanpawar2442005@gmail.com>
2026-02-28 10:33:45 +0530

    map fixed with better ui

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
 
-createRoot(document.getElementById('root')).render(
+import L from "leaflet";
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+
+// 🔧 FIX LEAFLET MARKER ICON ISSUE
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-<AuthProvider>
+    <AuthProvider>
       <App />
     </AuthProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
